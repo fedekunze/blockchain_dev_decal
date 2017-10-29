260ea18c95c278d56b11dbad2ed6b7e18026c79d
fedekunze <fekunze@berkeley.edu>
2017-10-29 13:07:07 -0700

    midterm ICO

---
+++ Midterm/contracts/Crowdsale.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.15;
+
+import './Queue.sol';
+import './Token.sol';
+
+/**
+ * @title Crowdsale
+ * @dev Contract that deploys `Token.sol`
+ * Is timelocked, manages buyer queue, updates balances on `Token.sol`
+ */
+
+contract Crowdsale {
+	// YOUR CODE HERE
+  /*
+  Join the Crowdsale
+  Buy
+
+  */
+
+  function Cowdsale() {
+
+  }
+
+
+  function () {
+    revert();
+  }
+
+}
